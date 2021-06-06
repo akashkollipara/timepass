e960c8a68904b33da3b4ba256bf181867df3eaa3
Akash Kollipara <akashkollipara@gmail.com>
2021-06-06 16:46:35 +0530

    minor changes

--- auto_run_function/src/main.c
+++ auto_run_function/src/main.c
@@ -5,10 +5,5 @@
 int main()
 {
 	auto_run();
-	printf("\n\n%ld\n", sizeof(size_t));
-	printf("\n\n%ld\n", sizeof(uintptr_t));
-	printf("\n\n%ld\n", sizeof(void *));
-	printf("\n\n%ld\n", sizeof(unsigned long));
-	printf("\n\n%ld\n", sizeof(long long));
 	return 0;
 }
